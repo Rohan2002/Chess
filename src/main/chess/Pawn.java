40fd760b214c43c6b4e0e493c675b366259b7c32
Saman Sathenjeri <saman.m.sath@gmail.com>
2023-10-16 15:42:18 -0400

    update promotion and enpassant

--- src/main/chess/Pawn.java
+++ src/main/chess/Pawn.java
@@ -3,8 +3,16 @@ package chess;
 import java.lang.Math;
 
 public class Pawn extends Piece {
+    char promo;
+    boolean enpassant;
     public Pawn(Color c, FileRank fr) {
         super(c, Piece.PieceType.pawn, fr);
+    }
+
+    //constructor if promotion is given with move
+    public Pawn(Color c, FileRank fr, char promotion) {
+        super(c, Piece.PieceType.pawn, fr);
+        promo = promotion;
     }
 
     @Override
@@ -30,17 +38,112 @@ public class Pawn extends Piece {
                     && nfr.getRank() - curr.getRank() <= allowed_moves;
             boolean blackPawnDownwards = this.getColorPiece() == Color.black && curr.getRank() - nfr.getRank() > 0
                     && curr.getRank() - nfr.getRank() <= allowed_moves;
-            
+
             boolean sameColumn = curr.getFile() == nfr.getFile();
+
+            //checks for promotion and conducts promotion
+            if (willPromote(b, nfr, nextPiece, curr, promo))
+            {
+                promotion(b, nfr, nextPiece, curr, promo);
+                return true;
+            }
+            /*else if (willEnpassant(b, nfr, nextPiece, curr, promo))
+            {
+                return true;
+            }*/
+            else if (nfr.getRank() - curr.getRank() == 2 && (sameColumn && (whitePawnUpwards || blackPawnDownwards)))
+            {
+                enpassant = true;
+            }
+
             return sameColumn && (whitePawnUpwards || blackPawnDownwards);
-        } else {
+        } 
+        else {
             // write attack policies here.
             FileRank nxt = nfr;
             boolean nxtPieceIsLeftOrRight = Math.abs(nxt.getFile() - curr.getFile()) == 1;
             boolean nxtPieceIsTop = Math.abs(nxt.getRank() - curr.getRank()) == 1;
             boolean oppositeColors = this.getColorPiece() != nextPiece.getColorPiece();
+            willPromote(b, nfr, nextPiece, curr, promo);
             return oppositeColors && nxtPieceIsLeftOrRight && nxtPieceIsTop;
         }
     }
 
+    public Piece promotion(Board b, FileRank nfr, Piece evalPiece, FileRank curr, char changedPiece)
+    {
+        switch (changedPiece)
+        {
+            case 'N':
+                return new Knight(evalPiece.getColorPiece(), nfr);
+
+            case 'R':
+                return new Rook(evalPiece.getColorPiece(), nfr);
+
+            case 'B':
+                return new Bishop(evalPiece.getColorPiece(), nfr);
+
+            case 'Q':
+                return new Queen(evalPiece.getColorPiece(), nfr);
+
+            default: 
+                return null;
+        }
+    }
+
+    public boolean willPromote(Board b, FileRank nfr, Piece evalPiece, FileRank curr, char changedPiece)
+    {
+        if ((this.getColorPiece() == Piece.Color.white && curr.getRank() == '8') || 
+                (this.getColorPiece() == Piece.Color.black && curr.getRank() == '1'))
+        {
+            promotion(b,nfr,this,curr,promo);
+            return true;
+        }
+        return false;
+    }
+
+    /*public boolean enpassant(Board b, FileRank nfr, Piece evalPiece, FileRank curr)
+    {
+        if ()
+        {
+
+        }
+        return true;
+    }
+
+    public boolean willEnpassant(Board b, FileRank nfr, Piece evalPiece, FileRank curr, char changedPiece)
+    {
+        char currFile = curr.getFile();
+        int currRank = curr.getRank(); 
+
+        char nextMoveFile = nfr.getFile();
+        int nextMoveRank = nfr.getRank();
+
+        Piece potentialEnpassantVictim = b.getPiece(new FileRank("" + ((char) (nfr.getFile()+1)) + (nfr.getRank())));
+
+        if (currRank == 5 && evalPiece.getColorPiece() == Piece.Color.white)
+        {
+            if (nfr.equals(new FileRank("" + ((char) (curr.getFile()+1)) + (curr.getRank()+1))) || 
+                nfr.equals(new FileRank("" + ((char) (curr.getFile()-1)) + (curr.getRank()+1))) )
+            {
+                if (potentialEnpassantVictim.getPieceType() == Piece.PieceType.pawn)
+                {
+                    if (((Pawn) potentialEnpassantVictim).enpassant == true)
+                    {
+                        enpassant(b, nfr, evalPiece, curr);
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void resetEnpassant(Board b, Color color)
+    {
+        for (Piece p : b.getAlivePieces()) {
+            if (p.getColorPiece() == color && p.getPieceType() == Piece.PieceType.pawn) {
+                ((Pawn) p).enpassant = false;
+            }
+        }
+    }*/
 }
---
+++ src/test/chess/TestPromotion.java
@@ -0,0 +1,50 @@
+package src.test.chess;
+
+import chess.Board;
+import chess.Piece;
+
+public class TestPromotion {
+    public static void main(String[] args) 
+    {
+        Board b = new Board();
+        b.initBoard();
+        System.out.println(b);
+
+        boolean test1_b = b.setPiece("g2", "g4"); // moves pawn away from Rook
+        assert test1_b == true;
+        System.out.println(b);
+
+        boolean test1_c = b.setPiece("g7", "g5"); // moves pawn away from Rook
+        assert test1_c == true;
+        System.out.println(b);
+        
+        boolean test1_a = b.setPiece("a1", "a4"); // Rook goes forward
+        assert test1_a == true;
+        System.out.println(b);
+
+        boolean test1_d = b.setPiece("e2", "e4"); // creates block
+        assert test1_d == true;
+        System.out.println(b);
+
+        boolean test2 = b.setPiece("a4", "h4"); //Rook goes to the right + Illegal Move
+        assert test2 == false;
+        System.out.println(b);
+
+        //boolean test3 = b.setPiece("h4", "a4"); // Rook goes to the left
+        boolean test3 = b.setPiece("a4", "d4"); // Rook goes to the left
+        assert test3 == true;
+        System.out.println(b);
+        
+        boolean test4 = b.setPiece("d4", "d3"); // Rook goes backward
+        assert test4 == true;
+        System.out.println(b);
+
+        boolean test5 = b.setPiece("d3", "e3"); // Rook goes up
+        assert test5 == true;
+        System.out.println(b);
+
+        boolean test6 = b.setPiece("e3", "e5"); // Illegal Move
+        assert test6 == false;
+        System.out.println(b);
+    }
+}
