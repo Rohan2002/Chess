a2b572b7e7d60cccbf7505821d2b41f6e96d8d58
Rohan2002 <rohandeshpande832@gmail.com>
2023-10-16 18:04:06 -0400

    refactor: promotion

--- src/main/chess/Pawn.java
+++ src/main/chess/Pawn.java
@@ -3,16 +3,9 @@ package chess;
 import java.lang.Math;
 
 public class Pawn extends Piece {
-    char promo;
     boolean enpassant;
     public Pawn(Color c, FileRank fr) {
         super(c, Piece.PieceType.pawn, fr);
-    }
-
-    //constructor if promotion is given with move
-    public Pawn(Color c, FileRank fr, char promotion) {
-        super(c, Piece.PieceType.pawn, fr);
-        promo = promotion;
     }
 
     @Override
@@ -41,20 +34,14 @@ public class Pawn extends Piece {
 
             boolean sameColumn = curr.getFile() == nfr.getFile();
 
-            //checks for promotion and conducts promotion
-            if (willPromote(b, nfr, nextPiece, curr, promo))
-            {
-                promotion(b, nfr, nextPiece, curr, promo);
-                return true;
-            }
             /*else if (willEnpassant(b, nfr, nextPiece, curr, promo))
             {
                 return true;
             }*/
-            else if (nfr.getRank() - curr.getRank() == 2 && (sameColumn && (whitePawnUpwards || blackPawnDownwards)))
-            {
-                enpassant = true;
-            }
+            // else if (nfr.getRank() - curr.getRank() == 2 && (sameColumn && (whitePawnUpwards || blackPawnDownwards)))
+            // {
+            //     enpassant = true;
+            // }
 
             return sameColumn && (whitePawnUpwards || blackPawnDownwards);
         } 
@@ -64,47 +51,8 @@ public class Pawn extends Piece {
             boolean nxtPieceIsLeftOrRight = Math.abs(nxt.getFile() - curr.getFile()) == 1;
             boolean nxtPieceIsTop = Math.abs(nxt.getRank() - curr.getRank()) == 1;
             boolean oppositeColors = this.getColorPiece() != nextPiece.getColorPiece();
-            willPromote(b, nfr, nextPiece, curr, promo);
             return oppositeColors && nxtPieceIsLeftOrRight && nxtPieceIsTop;
         }
-    }
-
-    public void promotion(Board b, FileRank nfr, Piece evalPiece, FileRank curr, char changedPiece)
-    {
-        switch (changedPiece)
-        {
-            case 'N':
-                b.getAlivePieces().add(new Knight(evalPiece.getColorPiece(), nfr));
-                break;
-
-            case 'R':
-                b.getAlivePieces().add(new Rook(evalPiece.getColorPiece(), nfr));
-                break;
-
-            case 'B':
-                b.getAlivePieces().add(new Bishop(evalPiece.getColorPiece(), nfr));
-                b.setAlivePieces(b.getAlivePieces());
-                break;
-
-            case 'Q':
-                b.getAlivePieces().add(new Queen(evalPiece.getColorPiece(), nfr));
-                break;
-
-            case 'P':
-                b.getAlivePieces().add(new Pawn(evalPiece.getColorPiece(), nfr));
-                break;
-        }
-    }
-
-    public boolean willPromote(Board b, FileRank nfr, Piece evalPiece, FileRank curr, char changedPiece)
-    {
-        if ((this.getColorPiece() == Piece.Color.white && nfr.getRank() == 8) || 
-                (this.getColorPiece() == Piece.Color.black && nfr.getRank() == 1))
-        {
-            promotion(b,nfr,this,curr,changedPiece);
-            return true;
-        }
-        return false;
     }
 
     /*public boolean enpassant(Board b, FileRank nfr, Piece evalPiece, FileRank curr)
